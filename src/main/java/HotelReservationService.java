17002102ad8ab68626f75cae3fb80ab6d90928f8
your name <exam@mail.com>
2021-04-05 05:10:27 +0530

    Added: Welcome message in master

---
+++ src/main/java/HotelReservationService.java
@@ -0,0 +1,6 @@
+public class HotelReservationService {
+
+    public static void main(String[] args) {
+            System.out.println("********* Welcome to Online Hotel Reservation Service ********");
+    }
+}
---
+++ src/test/java/HotelReservationTest.java
@@ -0,0 +1,55 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HotelReservationTest {
+
+    private HotelReservationService hotelReservationService;
+    private Hotel lakewood;
+    private Hotel bridgewood;
+    private Hotel ridgewood;
+    @Before
+    public void setUp() throws Exception {
+
+        hotelReservationService = new HotelReservationService();
+
+
+        HashMap<CustomerType, Rate> customerTypeRateMap = new HashMap<>();
+        customerTypeRateMap.put(CustomerType.REGULAR,new Rate(110,90));
+        customerTypeRateMap.put(CustomerType.REWARD, new Rate(80,80));
+        lakewood = new Hotel("Lakewood",3, customerTypeRateMap);
+
+        customerTypeRateMap = new HashMap<>();
+        customerTypeRateMap.put(CustomerType.REGULAR,new Rate(160,40));
+        customerTypeRateMap.put(CustomerType.REWARD, new Rate(110,50));
+        bridgewood = new Hotel("Bridgewood", 4, customerTypeRateMap);
+
+        customerTypeRateMap = new HashMap<>();
+        customerTypeRateMap.put(CustomerType.REGULAR,new Rate(220,150));
+        customerTypeRateMap.put(CustomerType.REWARD, new Rate(100,40));
+        ridgewood = new Hotel("Ridgewood", 5, customerTypeRateMap);
+        hotelReservationService.addHotel(lakewood);
+        hotelReservationService.addHotel(bridgewood);
+        hotelReservationService.addHotel(ridgewood);
+    }
+
+    @Test
+    public void givenHotel_whenInvokedAddHotel_shouldBeAbleToAdd() {
+        assertTrue(this.hotelReservationService.addHotel(lakewood));
+    }
+
+    @Test
+    public void givenDateRange_whenSearched_shouldReturnCheapestHotelBasedOnWeekdayAndWeekend() {
+        List<Result> cheapestHotelResult =
+                hotelReservationService.findCheapestHotelsBasedOnDay(CustomerType.REGULAR,
+                        "11Sep2020", "12Sep2020");
+
+        assertEquals(2,cheapestHotelResult.size());
+        assertEquals(200,cheapestHotelResult.get(0).getTotalRate());
+    }
+}
